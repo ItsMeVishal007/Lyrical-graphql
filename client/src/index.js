61109dd37e932e5faf252d2fe78aada7f084fdae
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-07-29 18:31:25 +0530

    setup the apollo client on the root of the frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-client";
+import { InMemoryCache, ApolloClient } from "@apollo/client";
 import { ApolloProvider } from "react-apollo";
 import "./index.css";
 import App from "./App";
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "http://localhost:4000/graphql",
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
