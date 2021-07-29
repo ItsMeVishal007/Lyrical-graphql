09255bb2910f6037b956ea1377c390e25972bc5f
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-07-29 19:40:09 +0530

    getting the data from graphql server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000",  //we can also put * to all any origin to access data
   })
 );
 
